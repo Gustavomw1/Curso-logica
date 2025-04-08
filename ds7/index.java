bed19d4c218b74051674756d8f78820b0990275a
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2025-04-08 17:12:15 -0300

    new aulas

---
+++ ds7/index.java
@@ -0,0 +1,11 @@
+public class index {
+    public static void main(String[] args) {
+        int salario = 6000;
+
+        if (salario > 4500) {
+            System.out.println(salario * 0.3);
+        } else {
+            System.out.println(salario * 0.15);
+        }
+    }
+}
