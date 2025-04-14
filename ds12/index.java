3d717b8a91174273eb91932f7b19bffc9058298f
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2025-04-14 17:03:58 -0300

    new aulas

---
+++ ds12/index.java
@@ -0,0 +1,17 @@
+package ds12;
+
+import java.util.Scanner;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite seu usuario: ");
+        String usuario = teclado.next();
+
+        if (usuario.equals("") || usuario.equalsIgnoreCase("admin") || usuario.equalsIgnoreCase("adiministrador")) {
+            System.out.println("(ERROR) Esse usuario não pode ser utilizado");
+        } else {
+            System.out.println("Usuario cadastrado com sucesso");
+        }
+    }
+}
---
+++ ds13/index.java
@@ -0,0 +1,21 @@
+package ds13;
+
+import java.util.Scanner;
+
+public class index {
+    public static void main(String[] args) {
+
+        char sexo = 'f';
+        int idade = 19;
+
+        if (sexo == 'm' && idade >= 18) {
+            System.out.println("Alistamento obrigatorio");
+        } else if (sexo == 'f' && idade > 18) {
+            System.out.println("Caso deseje se alistar ");
+        } else if (sexo == 'f' || sexo == 'm' && idade < 18) {
+            System.out.println("Alistamento nao obrigatorio");
+        } else {
+            System.out.println("(ERROR)");
+        }
+    }
+}
