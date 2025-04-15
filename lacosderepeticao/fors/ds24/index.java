72d4717a16d3779581492e6a7c3ebb9fa5d6864e
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2025-04-15 16:27:01 -0300

    new aulas

---
+++ lacosderepeticao/fors/ds24/index.java
@@ -0,0 +1,9 @@
+package lacosderepeticao.fors.ds24;
+
+public class index {
+    public static void main(String[] args) {
+        for (int i = 15; i <= 200; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
