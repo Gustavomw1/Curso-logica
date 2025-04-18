1260df733f9e160ddc448b061858c77c0a86f6f9
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2025-04-18 15:50:34 -0300

    new class

---
+++ arrays/ds30/index.java
@@ -0,0 +1,17 @@
+package arrays.ds30;
+
+public class index {
+    public static void main(String[] args) {
+        int[][] arrayMultil = new int[2][2];
+        arrayMultil[0][0] = 22;
+        arrayMultil[0][1] = 11;
+        arrayMultil[1][0] = 44;
+        arrayMultil[1][1] = 55;
+
+        System.out.println(arrayMultil[0][0]);
+        System.out.println(arrayMultil[0][1]);
+        System.out.println(arrayMultil[1][0]);
+        System.out.println(arrayMultil[1][1]);
+
+    }
+}
